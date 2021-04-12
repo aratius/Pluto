e0223123fad50220714984a15c5bf53d3dc8e9a8
aratius <arata1129matsu@icloud.com>
2021-04-12 22:35:21 +0900

    thetaをbiasに書き換えて同じことを

--- perceptron/and.py
+++ perceptron/and.py
@@ -1,12 +1,17 @@
+import numpy as np
+
 def AND(x1, x2):
   # w1,2 = weight, theta = threshold
   # x1,2 1 or 0 input
-  w1, w2, theta = 0.5, 0.5, 0.7  # there is infinite patern ex) 0.3, 0.3, 0.5
-  tmp = x1*w1 + x2*w2
-  if tmp <= theta:
+  x = np.array([x1, x2])
+  w = np.array([0.5, 0.5])
+  b = -0.7  # bias = -theta
+  tmp = np.sum(x * w) + b
+  if tmp <= 0:
     return 0
-  elif tmp > theta:
+  elif tmp > 0:
     return 1
+
 
 print(AND(0, 0))
 print(AND(1, 0))
