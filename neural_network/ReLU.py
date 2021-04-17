cbe7a0ecf8dee40b11cd2ec967b17fc9fb1b2977
aratius <arata1129matsu@icloud.com>
2021-04-17 15:37:49 +0900

    relu関数実装方法2

--- neural_network/ReLU.py
+++ neural_network/ReLU.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pylab as plt
 
 def ReLU(x):
-  return x * (x > 0.0)  # x > 0.0 expects 1 or 0
+  # return x * (x > 0.0)  # x > 0.0 expects 1 or 0
+  return np.maximum(0, x)
 
 x = np.arange(-5.0, 5.0, 0.1)
 y = ReLU(x)
