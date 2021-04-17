0af34495ef142b8d7c96380f3d32305becc2a1ee
aratius <arata1129matsu@icloud.com>
2021-04-17 17:05:21 +0900

    三層ニュートラルネットワーク一層目の計算と、活性化関数（シグモイド）を用いたZ（出力）の算出

---
+++ neural_network/neural_maxrix2.py
@@ -0,0 +1,11 @@
+import numpy as np
+from sigmoid_function import sigmoid
+
+X = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
+
+A1 = np.dot(X, W1) + B1
+print(A1)
+Z1 = sigmoid(A1)  # activation function
+print(Z1)
