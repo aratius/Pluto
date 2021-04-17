b7bff67505a822d841c021ee3bae59b1b2533ddc
aratius <arata1129matsu@icloud.com>
2021-04-17 17:27:20 +0900

    三層までの実装 それぞれの1のみ

--- neural_network/neural_maxrix3.py
+++ neural_network/neural_maxrix3.py
@@ -1,9 +1,8 @@
 import numpy as np
 from sigmoid_function import sigmoid
-from identity_function import identity_function
 
 # first ----------------------
-# definition
+# difinition
 X = np.array([1.0, 0.5])
 W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
 B1 = np.array([0.1, 0.2, 0.3])
@@ -15,7 +14,7 @@ print(Z1)
 
 
 # second ----------------------
-# definition
+# difinition
 W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
 B2 = np.array([0.1, 0.2])
 
@@ -23,14 +22,3 @@ A2 = np.dot(Z1, W2) + B2
 print(A2)
 Z2 = sigmoid(A2)
 print(Z2)
-
-# final
-# definition
-W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
-B3 = np.array([0.1, 0.2])
-
-A3 = np.dot(Z2, W3) + B3
-Y = identity_function(A3)
-
-print("output")
-print(Y)
