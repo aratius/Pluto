f214f3bf2e11806e894188012d3a9b08445c18fb
arata <aualrxse@icloud.com>
2021-07-12 14:28:17 +0900

    損失関数

---
+++ neural_network/loss_function.py
@@ -0,0 +1,36 @@
+
+import numpy as np
+
+# ２乗和誤差
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y - t)**2)
+
+# 交差エントロピー誤差
+def cross_entropy_error(y, t):
+    # 0に鳴らないための微小な値
+    delta = 1e-7
+    return - np.sum(t * np.log(y + delta))
+
+# 正解は２です (one_hot expression)
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+
+# 2が正解だと導き出した場合
+y1 = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+# ８が正解だと導き出した場合
+y2 = [0.1, 0.05, 0.0, 0.0, 0.05, 0.1, 0.0, 0.1, 0.6, 0.0]
+
+# 損失度合い（正解しているので少ない
+l1 = mean_squared_error(np.array(y1), np.array(t))
+# 損失度合い（不正解なので大きい
+l2 = mean_squared_error(np.array(y2), np.array(t))
+
+print(l1)
+print(l2)
+
+# 損失度合い（正解しているので少ない
+l3 = cross_entropy_error(np.array(y1), np.array(t))
+# 損失度合い（不正解なので大きい
+l4 = cross_entropy_error(np.array(y2), np.array(t))
+
+print(l3)
+print(l4)
--- perceptron/step_function.py
+++ perceptron/step_function.py
@@ -1,9 +1,13 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import matplotlib.pylab as plt
 
+# ステップ関数
 def step_function(x):
   return np.array(x > 0, dtype=np.int)
 
+# -5 ~ 5 で 0.1 刻みのnumpy配列
 x = np.arange(-5.0, 5.0, 0.1)
 y = step_function(x)
 
