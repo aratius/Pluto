4cf6e4939b504dd964d8a4bbafc0b6e3297cb006
aratius <arata1129matsu@icloud.com>
2021-07-17 20:57:22 +0900

    レイヤ演習

---
+++ propagation/softmax-with-loss.py
@@ -0,0 +1,28 @@
+import sys, os
+sys.path.append("/Users/matsumotoarata/git/ME/Python/deep-learning")  # 親ディレクトリのファイルをインポートするための設定
+from neural_network.softmax import softmax
+from common.functions import cross_entropy_error
+import numpy as np
+
+# softmax-with-lossレイヤ
+class SoftmaxWithLoss:
+  def __init__(self):
+    self.loss = None
+    self.y = None
+    self.t = None
+
+  def forward(self, x, t):
+    self.t = t
+    self.y = softmax(x)
+    self.loss = cross_entropy_error(self.y, self.t)
+
+    return self.loss
+
+  # 出力層なのでdout=1と初期値を設定できる
+  # 流す値は y - t (正解ラベルとの誤差)になる
+  def backward(self, dout=1):
+    batch_size = self.t.shape[0]
+    dx = (self.y - self.t) / batch_size
+
+    return dx
+
